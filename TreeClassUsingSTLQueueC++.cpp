938cc7c55559112c9fe92f829d8d2c4cb631a840
Siddharth Singh <siddharthiiitg@gmail.com>
2022-06-10 19:03:52 +0530

    Tree ds implementation

---
+++ TreeClassUsingSTLQueueC++.cpp
@@ -0,0 +1,213 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+ 
+class Node{
+public:
+    Node* lchild;
+    int data;
+    Node* rchild;
+};
+ 
+class Tree{
+private:
+    Node* root;
+public:
+    Tree();
+    ~Tree();
+    void CreateTree();
+    void Preorder(Node* p);
+    void Preorder() { Preorder(root); }  // Passing Private Parameter in Constructor
+    void Inorder(Node* p);
+    void Inorder() { Inorder(root); }
+    void Postorder(Node* p);
+    void Postorder() { Postorder(root); }
+    void Levelorder(Node* p);
+    void Levelorder() { Levelorder(root); }
+    int Height(Node* p);
+    int Height() { return Height(root); }
+    int CountNodes(Node* p);
+    int CountNodes() { return CountNodes(root); }
+    int CountLeafNodes(Node* p);
+    int CountLeafNodes() { return CountLeafNodes(root); }
+    int Sum(Node* p);
+    int Sum() { return Sum(root); }
+};
+ 
+Tree::Tree() {
+    root = nullptr;
+}
+ 
+Tree::~Tree() {
+    // TODO
+}
+ 
+void Tree::CreateTree() {
+    Node* p;
+    Node* t;
+    int x;
+    queue<Node*> q;
+ 
+    root = new Node;
+    cout << "Enter root data: " << flush;
+    cin >> x;
+    root->data = x;
+    root->lchild = nullptr;
+    root->rchild = nullptr;
+    q.emplace(root);
+ 
+    while (! q.empty()){
+        p = q.front();
+        q.pop();
+ 
+        cout << "Enter left child data of " << p->data << ": " << flush;
+        cin >> x;
+        if (x != -1){
+            t = new Node;
+            t->data = x;
+            t->lchild = nullptr;
+            t->rchild = nullptr;
+            p->lchild = t;
+            q.emplace(t);
+        }
+ 
+        cout << "Enter right child data of " << p->data << ": " << flush;
+        cin >> x;
+        if (x != -1){
+            t = new Node;
+            t->data = x;
+            t->lchild = nullptr;
+            t->rchild = nullptr;
+            p->rchild = t;
+            q.emplace(t);
+        }
+    }
+}
+ 
+void Tree::Preorder(Node *p) {
+    if (p){
+        cout << p->data << ", " << flush;
+        Preorder(p->lchild);
+        Preorder(p->rchild);
+    }
+}
+ 
+void Tree::Inorder(Node *p) {
+    if (p){
+        Inorder(p->lchild);
+        cout << p->data << ", " << flush;
+        Inorder(p->rchild);
+    }
+}
+ 
+void Tree::Postorder(Node *p) {
+    if (p){
+        Postorder(p->lchild);
+        Postorder(p->rchild);
+        cout << p->data << ", " << flush;
+    }
+}
+ 
+void Tree::Levelorder(Node *p) {
+    queue<Node*> q;
+    cout << root->data << ", " << flush;
+    q.emplace(root);
+ 
+    while (! q.empty()){
+        p = q.front();
+        q.pop();
+ 
+        if (p->lchild){
+            cout << p->lchild->data << ", " << flush;
+            q.emplace(p->lchild);
+        }
+ 
+        if (p->rchild){
+            cout << p->rchild->data << ", " << flush;
+            q.emplace(p->rchild);
+        }
+    }
+}
+ 
+int Tree::Height(Node *p) {
+    int l = 0;
+    int r = 0;
+    if (p == nullptr){
+        return 0;
+    }
+ 
+    l = Height(p->lchild);
+    r = Height(p->rchild);
+ 
+    if (l > r){
+        return l + 1;
+    } else {
+        return r + 1;
+    }
+}
+
+int Tree::CountNodes(Node * p){
+    if(p){
+        int l = CountNodes(p->lchild);
+        int r = CountNodes(p->rchild);
+
+        return l+r+1;
+    }
+
+    return 0;
+}
+ 
+int Tree::CountLeafNodes(Node *p){  
+    if(p){
+        int l = CountLeafNodes(p->lchild);
+        int r = CountLeafNodes(p->rchild);
+
+        if(p->lchild || p->rchild) return l+r;
+        return l+r+1;
+    }
+    return 0;
+}
+
+
+int Tree::Sum(Node * p){
+    if(p){
+        int lsum = Sum(p->lchild);
+        int rsum = Sum(p->rchild);
+
+        return lsum + rsum + p->data;
+    }
+
+    return 0;
+}
+
+
+int main() {
+ 
+    Tree bt;
+ 
+    bt.CreateTree();
+    cout << endl;
+ 
+    cout << "Preorder: " << flush;
+    bt.Preorder();
+    cout << endl;
+ 
+    cout << "Inorder: " << flush;
+    bt.Inorder();
+    cout << endl;
+ 
+    cout << "Postorder: " << flush;
+    bt.Postorder();
+    cout << endl;
+ 
+    cout << "Levelorder: " << flush;
+    bt.Levelorder();
+    cout << endl;
+ 
+    cout << "Height: " << bt.Height() << endl;
+
+    cout << "No of leaf nodes: " << bt.CountLeafNodes() << endl;
+ 
+    return 0;
+}
